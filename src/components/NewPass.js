b79563414638531d5c7987be5b2730a850b69372
Eric <ericd.perin@gmail.com>
2023-10-20 10:14:02 -0400

    Added password reset with email auth

--- src/components/NewPass.js
+++ src/components/NewPass.js
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/NewPass.css';
-import logostem from '../images/logostem.png'
-
+import logostem from '../images/logostem.png';
+import 'firebase/auth';
+import { auth } from '../Config/firebase.js';
+import { sendPasswordResetEmail } from 'firebase/auth';
 
 function NewPass() {
+  const [email, setEmail] = useState('');
+  const [mensagem, setMensagem] = useState('');
+  const [mensagemTipo, setMensagemTipo] = useState('');
+
+  function recuperarSenha() {
+    sendPasswordResetEmail(auth, email)
+      .then((resultado) => {
+        alert('Email enviado com sucesso.');
+        setMensagemTipo('success');
+        setMensagem('Email enviado com sucesso.');
+      })
+      .catch((erro) => {
+        setMensagem('Erro ao enviar o email: ' + erro.message);
+        setMensagemTipo('error');
+        alert('Erro ao enviar o email: ' + erro.message);
+      });
+  }
+
   return (
-    <div className = "centerBody">
+    <div className="centerBody">
+      <img src={logostem} alt="Perfil" className="top-left-image" />
+      <div>
+        <h1 className="title">Redefinir senha</h1>
 
-      <img src = {logostem} alt="Perfil" className="top-left-image" />
-      <div >
-      <h1 className = "title" >Redefinir senha</h1>
+        <input
+          type="email"
+          placeholder="E-mail"
+          className="input"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-  
-
-      <input type="text" placeholder="e-mail" />
-
-      <input type="password" placeholder="Nova senha" />
-
-      <input type="password" placeholder="Confirmar nova senha" />
-
-      <input type="button" value="Redefinir" className = "register-button"/>
-
-      <Link to="/login">Voltar para o login</Link>
-
+        <button onClick={recuperarSenha} type="button" className="register-button">
+          Redefinir
+        </button>
+        {mensagem && (
+          <div className={`alert ${mensagemTipo}`}>
+            {mensagem}
+          </div>
+        )}
+        <Link to="/login">Voltar para o login</Link>
       </div>
     </div>
   );
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,95 +1,55 @@
 import React, { useState } from 'react';
-import '../styles/RegisterForm.css';
-import logostem from '../images/logostem.png';
 import { Link } from 'react-router-dom';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import '../styles/NewPass.css';
+import logostem from '../images/logostem.png';
 import 'firebase/auth';
 import { auth } from '../Config/firebase.js';
+import { sendPasswordResetEmail } from 'firebase/auth';
 
-function RegisterForm() {
+function NewPass() {
   const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-  const [confirmSenha, setConfirmSenha] = useState('');
-  const [nome, setNome] = useState(''); // Adicionando estado para o nome
   const [mensagem, setMensagem] = useState('');
-  const [mensagemTipo, setMensagemTipo] = useState(''); // 'success' ou 'error'
+  const [mensagemTipo, setMensagemTipo] = useState('');
 
-  const RegisterUser = (e) => {
-    e.preventDefault();
-
-    if (senha !== confirmSenha) {
-      setMensagem('As senhas não coincidem');
-      setMensagemTipo('error');
-      return;
-    }
-
-    createUserWithEmailAndPassword(auth, email, senha)
-      .then((UserCredential) => {
-        // Você pode acessar o ID do usuário recém-criado em UserCredential.user.uid
-        console.log(UserCredential);
-        setMensagem('Cadastro realizado com sucesso!');
+  function recuperarSenha() {
+    sendPasswordResetEmail(auth, email)
+      .then((resultado) => {
+        alert('Email enviado com sucesso.');
         setMensagemTipo('success');
+        setMensagem('Email enviado com sucesso.');
       })
-      .catch((error) => {
-        if (error.message === 'Firebase: Password should be at least 6 characters') {
-          setMensagemTipo('A senha deve ter pelo menos 6 caracteres');
-          setMensagemTipo('error');
-        } else if (error.message === 'The email is badly formatted.') {
-          setMensagemTipo('O email não é válido');
-          setMensagemTipo('error');
-        } else if (error.message === 'The email address is already in use by another account') {
-          setMensagem('Esse email já está em uso');
-          setMensagemTipo('error');
-        } else {
-          setMensagem('Erro ao criar conta: ' + error.message);
-          setMensagemTipo('error');
-        }
+      .catch((erro) => {
+        setMensagem('Erro ao enviar o email: ' + erro.message);
+        setMensagemTipo('error');
+        alert('Erro ao enviar o email: ' + erro.message);
       });
-  };
+  }
 
   return (
     <div className="centerBody">
       <img src={logostem} alt="Perfil" className="top-left-image" />
+      <div>
+        <h1 className="title">Redefinir senha</h1>
 
-      <form className="form" onSubmit={RegisterUser}>
-        <h1 className="title">Cadastro</h1>
-
-        <input
-          type="text" // Alterado para tipo "text" para o campo de nome
-          placeholder="Nome"
-          className="input"
-          onChange={(e) => setNome(e.target.value)} // Armazena o nome no estado
-        />
         <input
           type="email"
           placeholder="E-mail"
           className="input"
           onChange={(e) => setEmail(e.target.value)}
         />
-        <input
-          type="password"
-          placeholder="Senha"
-          className="input"
-          onChange={(e) => setSenha(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Confirmação da senha"
-          className="input"
-          onChange={(e) => setConfirmSenha(e.target.value)}
-        />
-        <button className="register-button">Cadastrar</button>
-        <p>
-          Possui conta? Fazer <Link to="/login">login</Link>
-        </p>
+
+        <button onClick={recuperarSenha} type="button" className="register-button">
+          Redefinir
+        </button>
         {mensagem && (
           <div className={`alert ${mensagemTipo}`}>
             {mensagem}
           </div>
         )}
-      </form>
+        <Link to="/login">Voltar para o login</Link>
+      </div>
     </div>
   );
 }
 
-export default RegisterForm;
+export default NewPass;
