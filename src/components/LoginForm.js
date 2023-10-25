8cfe26cf2ead60a08188c850c1cd83612f7f07b7
Eric <ericd.perin11@gmail.com>
2023-10-24 21:53:39 -0400

    correcting errors

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,55 +1,70 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/NewPass.css';
+import '../styles/LoginForm.css';
 import logostem from '../images/logostem.png';
+import { useNavigate } from 'react-router';
+import { Link } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import 'firebase/auth';
 import { auth } from '../Config/firebase.js';
-import { sendPasswordResetEmail } from 'firebase/auth';
 
-function NewPass() {
+function LoginForm() {
   const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
   const [mensagem, setMensagem] = useState('');
-  const [mensagemTipo, setMensagemTipo] = useState('');
+  const [mensagemTipo, setMensagemTipo] = useState(''); // 'success' ou 'error'
 
-  function recuperarSenha() {
-    sendPasswordResetEmail(auth, email)
-      .then(() => {
-        setMensagem('Email enviado com sucesso.');
+  const navigate = useNavigate(); // Inicialize o useHistory
+
+  const LoginUser = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, senha)
+      .then((UserCredential) => {
+        console.log(UserCredential);
+        setMensagem('Login válido');
         setMensagemTipo('success');
+        navigate('/mainpage'); 
       })
-      .catch((erro) => {
-        setMensagem('Erro ao enviar o email: ' + erro.message);
-        setMensagemTipo('error');
+      .catch((error) => {
+        console.log('Erro:', error);
+        setMensagem(error.message); // Usar a mensagem de erro do Firebase
+        setMensagemTipo('Ocorreu um erro ao fazer o login: ' + 'error');
       });
-  }
+  };
 
   return (
-    <div className="centerBody">
+    <div className="login-component">
       <img src={logostem} alt="Perfil" className="top-left-image" />
-      <div>
-        <h1 className="title">Redefinir senha</h1>
+      <div className="center-content" id="form">
+        <h1 className="center-title"><i>EDULAB</i></h1>
 
-        <input
-          type="email"
-          placeholder="E-mail"
-          className="input"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <button onClick={recuperarSenha} type="button" className="register-button">
-          Redefinir
-        </button>
-
-        {mensagem && (
-          <div className={`alert ${mensagemTipo}`}>
-            {mensagem}
-          </div>
-        )}
-
-        <Link to="/login">Voltar para o login</Link>
+        <form className="form" onSubmit={LoginUser}>
+          <input
+            type="email"
+            placeholder="E-mail"
+            className="input"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            className="input"
+            onChange={(e) => setSenha(e.target.value)}
+          />
+          <button className="login-button">Login</button>
+          <Link to="/novasenha">Esqueceu sua senha?</Link>
+          <p>
+            Não tem conta? <Link to="/cadastro">Cadastrar</Link>
+          </p>
+          {mensagem && (
+            <div className={`alert ${mensagemTipo}`}>
+              {mensagem}
+            </div>
+          )}
+        </form>
       </div>
     </div>
   );
 }
 
-export default NewPass;
+export default LoginForm;
